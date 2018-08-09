510d8b3f30ce31f37e304f7bfa3b7c822be0586e
Brandon Patterson <brandnpatterson@gmail.com>
2018-08-09 14:21:59 -0500

    add tests - files

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import App from '../App';
-import CommentBox from '../components/CommentBox';
+import App from 'App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
 
 it('shows the comment box', () => {
-  const wrapped = shallow(<App />);
-
   expect(wrapped.find(CommentBox).length).toEqual(1);
+});
+
+it('shows the comment list', () => {
+  expect(wrapped.find(CommentList).length).toEqual(1);
 });
--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default () => {
-  return (
-    <div>
-      <h2>Comment Box</h2>
-    </div>
-  );
-};
+class CommentBox extends Component {
+  state = {
+    comment: ''
+  };
+
+  onChange = e => {
+    this.setState({
+      comment: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    // @todo call an action and save the comment
+    this.setState({ comment: '' });
+  };
+
+  render() {
+    return (
+      <form onSubmit={this.onSubmit}>
+        <h4>Add a Comment</h4>
+        <textarea value={this.state.comment} onChange={this.onChange} />
+        <div>
+          <button>Submit</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default CommentBox;
