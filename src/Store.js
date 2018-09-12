f66966ba81c5f870ecb2fa467dbb085b3b42d3f2
Brandon Patterson <brandnpatterson@gmail.com>
2018-09-11 20:00:23 -0500

    added redux devtools

--- src/Store.js
+++ src/Store.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { applyMiddleware, compose, createStore } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
 // store
-import { createStore } from 'redux';
 import reducers from './reducers';
 
 const initialState = {};
-const store = createStore(reducers, initialState);
+const middleware = [thunk];
+
+let store;
+
+if (process.env.NODE_ENV === 'development') {
+  store = createStore(
+    reducers,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+} else if (process.env.NODE_ENV === 'production') {
+  store = createStore(
+    reducers,
+    initialState,
+    compose(applyMiddleware(...middleware))
+  );
+}
 
 const Store = props => {
   return <Provider store={store}>{props.children}</Provider>;
